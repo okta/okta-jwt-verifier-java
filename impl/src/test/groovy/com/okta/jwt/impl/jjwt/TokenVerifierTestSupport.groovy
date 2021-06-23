cfa60d0747abd2c7c9311c969b9abaf78b5909d8
Arvind Krishnakumar <arvind.krishnakumar@okta.com>
2021-06-23 08:41:45 -0700

    add none algo UT

--- impl/src/test/groovy/com/okta/jwt/impl/jjwt/TokenVerifierTestSupport.groovy
+++ impl/src/test/groovy/com/okta/jwt/impl/jjwt/TokenVerifierTestSupport.groovy
@@ -124,6 +124,14 @@ abstract class TokenVerifierTestSupport {
     }
 
     @Test
+    void noneAlgoTest() {
+        def token = buildJwtWithFudgedHeader('{"alg": "none"}')
+
+        expect JwtVerificationException, {
+            decodeToken(token)
+        }
+    }
+    @Test
     void nullAudienceTest() {
         expect JwtVerificationException, {
             buildThenDecodeToken(baseJwtBuilder()
